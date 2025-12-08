c83e033eb5ffcc7e090cd60f1a02b1ca10630ad8
Afraz Mishak <afrazmishak@gmail.com>
2025-12-08 18:28:55 +0400

    ToDoList - <Input /> included and correction pending

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -2,13 +2,20 @@ import { useState } from "react"
 
 export function ToDoList () {
     const [todo, setTodo] = useState(['Buy Milk'])
+    const [textAddToDo, setTextAddToDo] = useState('')
 
     const addTodo = newTodo => setTodo(prev => [...prev, newTodo])
     const removeFirst = () => setTodo(prev => prev.slice(1))
 
     return (
         <div>
-            <button onClick={() => addTodo('New item')}>Add</button>
+            <input 
+                type="text" 
+                value={textAddToDo}
+                onChange={(e) => setTextAddToDo(e.target.value)}
+            />  
+
+            <button onClick={() => addTodo(textAddToDo)}>Add</button>
             <button onClick={removeFirst}>Remove First</button>
             <ul>{todo.map((t, i) => <li key={i}>{t}</li>)}</ul>
         </div>
