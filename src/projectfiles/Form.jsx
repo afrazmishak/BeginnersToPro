4a85eb16f2fd722058322f7ec08100ca2ed2ef6f
Afraz Mishak <afrazmishak@gmail.com>
2025-12-09 17:53:14 +0400

    The Form updated to submit the firstName and lastName

--- src/projectfiles/Form.jsx
+++ src/projectfiles/Form.jsx
@@ -2,23 +2,54 @@ import { useState } from "react"
 
 export function Form() {
     const [name, setName] = useState([]);
-    const [textAddName, setTextAddName] = useState('')
+    const [formData, setFormData] = useState({
+        firstName: '',
+        lastName: ''
+    })
 
-    const addNewName = (newName) => {
-        if (!newName.trim()) return;
-        setName(prev => [...prev, newName])
-        setTextAddName('')
+    const addNewName = () => {
+        if(!formData.firstName.trim() || !formData.lastName.trim()) return;
+
+        setName(prev => [...prev, formData])
+        
+        setFormData({
+            firstName: '',
+            lastName: ''
+        })
     }
 
     return (
         <>
-            <input type="text" value={textAddName} onChange={(e) => setTextAddName(e.target.value)} />
-            
-            <button onClick={() => addNewName((textAddName))}>Submit</button>
+
+            <input
+                type="text"
+                placeholder="First Name"
+                value={formData.firstName}
+                onChange={(e) =>
+                    setFormData(prev => ({
+                        ...prev,
+                        firstName: e.target.value
+                    }))
+                }
+            />
+
+            <input 
+                type="text" 
+                placeholder="Last Name"
+                value={formData.lastName}
+                onChange={(e) =>
+                    setFormData(prev => ({
+                        ...prev,
+                        lastName: e.target.value
+                    }))
+                }
+            />
+
+            <button onClick={addNewName}>Submit</button>
 
             <ul>
                 {name.map((n, i) => (
-                    <li key={i}>{n}</li>
+                    <li key={i}>{n.firstName} {n.lastName}</li>
                 ))}
             </ul>
         </>
