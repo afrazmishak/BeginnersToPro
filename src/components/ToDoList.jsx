c09f16df70c8b91ab9d9ffb3d10ef0d2b5508392
Afraz Mishak <afrazmishak@gmail.com>
2025-12-09 12:23:35 +0400

    ToDoList - Clear <Input /> after clicking on adding button

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,23 +1,31 @@
 import { useState } from "react"
 
-export function ToDoList () {
+export function ToDoList() {
     const [todo, setTodo] = useState(['Buy Milk'])
     const [textAddToDo, setTextAddToDo] = useState('')
 
-    const addTodo = newTodo => setTodo(prev => [...prev, newTodo])
+    const addTodo = (newTodo) => {
+        if (!newTodo.trim()) return
+        setTodo(prev => [...prev, newTodo])
+        setTextAddToDo('')
+    }
     const removeFirst = () => setTodo(prev => prev.slice(1))
 
     return (
         <div>
-            <input 
-                type="text" 
+            <input
+                type="text"
                 value={textAddToDo}
                 onChange={(e) => setTextAddToDo(e.target.value)}
-            />  
+            />
 
             <button onClick={() => addTodo(textAddToDo)}>Add</button>
             <button onClick={removeFirst}>Remove First</button>
-            <ul>{todo.map((t, i) => <li key={i}>{t}</li>)}</ul>
+            <ul>
+                {todo.map((t, i) => (
+                    <li key={i}>{t}</li>
+                ))}
+            </ul>
         </div>
     )
 }
